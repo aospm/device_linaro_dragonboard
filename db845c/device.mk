472e081fe3430a9e976d997f02af3f6941862186
Amit Pundir <amit.pundir@linaro.org>
2021-12-14 19:45:56 +0530

    db845c: Add rb5 support in db845c-userdebug build target
    * Enabled multile appended DTBs support to allow db845c
    boot.img/vendor_boot.img to boot on rb5 as well.
    
    * Introduced a vendor.hw property which will be used to run
    device specific services. e.g. setting correct HDMI mixer
    control to begin with.
    
    TODO: Figure out the correct way to set mixer control paths
    in mixer_path.xml based on the device card names
    (/proc/asound/cards). Falling back to tinymix for the time
    being.
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Change-Id: I779d01946ae359d5c405d959419e29460083e5df

--- db845c/device.mk
+++ db845c/device.mk
@@ -33,7 +33,6 @@ endif
 
 PRODUCT_COPY_FILES := \
     $(DB845C_KERNEL_DIR)/Image.gz:kernel \
-    $(DB845C_KERNEL_DIR)/sdm845-db845c.dtb:dtb.img \
     device/linaro/dragonboard/fstab.common:$(TARGET_COPY_OUT_RAMDISK)/fstab.db845c \
     device/linaro/dragonboard/fstab.common:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.db845c \
     device/linaro/dragonboard/init.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.db845c.rc \
@@ -49,8 +48,9 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl.recovery \
     android.hardware.boot@1.2-service
 
-# Install script to set *.usb.controller properties
+# Install scripts to set vendor.* properties
 PRODUCT_COPY_FILES += \
+    device/linaro/dragonboard/qcom/set_hw.sh:$(TARGET_COPY_OUT_VENDOR)/bin/set_hw.sh \
     device/linaro/dragonboard/qcom/set_udc.sh:$(TARGET_COPY_OUT_VENDOR)/bin/set_udc.sh
 
 # Install scripts to set Ethernet MAC address
@@ -65,3 +65,4 @@ PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=160
 
 # Copy firmware files
 $(call inherit-product-if-exists, vendor/linaro_devices/qcom/db845c/device.mk)
+$(call inherit-product-if-exists, vendor/linaro_devices/qcom/rb5/device.mk)
